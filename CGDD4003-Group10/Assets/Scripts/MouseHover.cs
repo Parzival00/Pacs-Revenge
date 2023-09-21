5dee9f3abf05b8eee8c3f4fd995022ac15506ca5
Brandon-Dominguez <bdoming3@students.kennesaw.edu>
2023-09-20 20:51:58 -0400

    Menu Update
    Current Menu set up

---
+++ CGDD4003-Group10/Assets/Scripts/MouseHover.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class MouseHover : MonoBehaviour
+{
+    private Color startcolor;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
