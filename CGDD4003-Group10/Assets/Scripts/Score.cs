7a9c4cd0626eaee5a07d240fafa976617c17f196
Ben Hedges <benjoehedges@gmail.com>
2023-09-26 17:17:57 -0400

    Cherries and win condition

--- CGDD4003-Group10/Assets/Scripts/Score.cs
+++ CGDD4003-Group10/Assets/Scripts/Score.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class Score : MonoBehaviour
 {
@@ -11,13 +12,20 @@ public class Score : MonoBehaviour
     [SerializeField] TMP_Text scoreUI;
     [SerializeField] AudioSource audioSource;
     [SerializeField] AudioClip pelletSound;
+    [SerializeField] GameObject cherryObject;
+    [SerializeField] int cherrySpawn1, cherrySpawn2;
+
+    private int totalPellets;
     void Start()
     {
         pelletsCollected = 0;
         score = 0;
+        GameObject[] pellets = GameObject.FindGameObjectsWithTag("Pellet");
+        totalPellets = pellets.Length;
+        cherryObject.SetActive(false);
     }
 
-    
+
     void Update()
     {
         UpdateScore();
@@ -48,13 +56,21 @@ public class Score : MonoBehaviour
             pelletsCollected += 1;
             //Destroy(other.gameObject);
             score += 50;
+            if (pelletsCollected >= totalPellets)
+            {
+                SceneManager.LoadScene(2);
+            }
+            else if (pelletsCollected == cherrySpawn1 || pelletsCollected == cherrySpawn2)
+            {
+                cherryObject.SetActive(true);
+            }
         }
 
         if (other.gameObject.tag == "Cherry")
         {
             other.gameObject.SetActive(false);
             //Destroy(other.gameObject);
-            score += 100;
+            score += 1000;
         }
     }
 
