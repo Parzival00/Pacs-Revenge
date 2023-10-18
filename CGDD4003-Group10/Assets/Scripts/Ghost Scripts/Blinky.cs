cbb852c15d61f57b57e4817e54fea6023305e310
Quantum-Memester <toby.mose@gmail.com>
2023-10-18 12:32:10 -0400

    Blinky AI Changes
    Added a timed turn around to blinky's AI during scatter

--- CGDD4003-Group10/Assets/Scripts/Ghost Scripts/Blinky.cs
+++ CGDD4003-Group10/Assets/Scripts/Ghost Scripts/Blinky.cs
@@ -4,9 +4,63 @@ using UnityEngine;
 
 public class Blinky : Ghost
 {
+    [Header("Blinky specific settings")]
+    [SerializeField] float cooldownTime;
+    protected float cooldownTimer;
+    protected bool canTurn;
     //Blinky's chase is the simplest being go to the player location so just uses base functionality of Chase()
     protected override void Chase()
     {
         base.Chase();
     }
+
+    protected override void Scatter()
+    {
+        Vector2Int playerGridPosition = map.CheckEdgePositions(transform.position);
+
+        targetGridPosition = playerGridPosition;
+
+        if(canTurn)
+        {
+            canTurn = !Move(true);
+            //This happens if blinky turned in the last move call
+            if(!canTurn)
+            {
+                cooldownTimer = cooldownTime;
+                print("turned");
+            }
+        }
+        else 
+        {
+            Move(false);
+            if (cooldownTimer > 0)
+            {
+                cooldownTimer -= Time.deltaTime;    
+            }
+            else
+            {
+                canTurn = true;
+            }
+        }
+        lastTargetGridPosition = targetGridPosition;
+        PlayChaseSound();
+    }
+    public override void InitiateScatter()
+    {
+        if (currentMode == Mode.Chase)
+        {
+            currentMode = Mode.Scatter;
+            canTurn = false;
+            cooldownTimer = cooldownTime;
+        }
+    }
+    public override void DeactivateScatter()
+    {
+        if (currentMode == Mode.Scatter)
+        {
+            currentMode = Mode.Chase;
+            canTurn = false;
+        }
+    }
+
 }
