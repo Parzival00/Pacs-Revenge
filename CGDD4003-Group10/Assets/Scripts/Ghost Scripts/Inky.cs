7badb166ad50c1b560e6b0e8c59c6d76615324f6
Quantum-Memester <toby.mose@gmail.com>
2023-10-18 11:38:27 -0400

    Inky AI Change
    In Theory the Inky dash mechanic is working. It still hunts the player when the weapon is active and will gain a speed boost when it's cooldown is 0

--- CGDD4003-Group10/Assets/Scripts/Ghost Scripts/Inky.cs
+++ CGDD4003-Group10/Assets/Scripts/Ghost Scripts/Inky.cs
@@ -7,6 +7,14 @@ public class Inky : Ghost
     [Header("Inky Specific Settings")]
     [SerializeField] Transform blinkyTransform;
     [SerializeField] float targetRayMultiplier = 1.5f;
+    [SerializeField] protected float dashSpeedMultiplier;
+    [SerializeField] protected float dashTime;
+    [SerializeField] protected float dashCooldown;
+    protected float cooldownTimer;
+    protected float dashTimer;
+    protected bool canDash;
+    protected bool dashing;
+    protected float baseSpeed;
 
     //Temporary Movement to player
     protected override void Chase()
@@ -34,7 +42,61 @@ public class Inky : Ghost
 
         spriteRenderer.color = Color.white; //Temporary
     }
+    protected override void Scatter()
+    {
+        if (dashing && dashTimer <= 0)
+        {
+            //ends the dash
+            dashing = false;
+            cooldownTimer = dashCooldown;
+            speed = baseSpeed;
+        }
+        else if (canDash && !dashing && cooldownTimer <= 0) 
+        {
+            //starts the dash
+            print("Dashing");
+            speed *= dashSpeedMultiplier;
+            dashing = true;
+            canDash = false;
+            dashTimer = dashTime;
+        }
+        else if (dashing && dashTimer > 0)
+        {
+            dashTimer -= Time.deltaTime;
+        }
 
+        if (cooldownTimer > 0)
+        {
+            cooldownTimer -= Time.deltaTime;
+        }
+        else
+        {
+            canDash = true;
+            cooldownTimer = 0;
+        }
+
+        Chase();
+    }
+    public override void InitiateScatter()
+    {
+        if (currentMode == Mode.Chase)
+        {
+            baseSpeed = speed;
+            currentMode = Mode.Scatter;
+            dashTimer = 0;
+            cooldownTimer = dashCooldown;
+        }
+    }
+    public override void DeactivateScatter()
+    {
+        if (currentMode == Mode.Scatter)
+        {
+            speed = baseSpeed;
+            canDash = false;
+            dashing = false;
+            currentMode = Mode.Chase;
+        }
+    }
     ///Summary
     ///Take Blinky distance to player || do rand in between those position to set inky
     ///Summary
